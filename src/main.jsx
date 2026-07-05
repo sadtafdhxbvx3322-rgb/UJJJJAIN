411537f708493af7234f17d28f63c4fe97e25a77
sadtafdhxbvx3322-rgb <sadtafdhxbvx3322@gmail.com>
2026-07-05 23:43:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, message: "" };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true, message: error.message };
+  }
+  render() {
+    if (this.state.hasError) {
+      return <div style={{ color: 'red', padding: '30px', background: 'black', height: '100vh' }}>
+        <h2>React Crashed!</h2><p>{this.state.message}</p>
+      </div>;
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
